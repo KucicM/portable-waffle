7defe4dc3a6afaba6cd9ffda2d04d4703532f96b
kucicm <10778553+KucicM@users.noreply.github.com>
2022-04-19 21:13:49 +0200

    install neovim

---
+++ nuke.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf ~/.config/nvim 
+sudo rm /usr/local/bin/nvim
+sudo rm -r /usr/local/share/nvim/
+sudo rm -rf /home/repos/neovim
+rm -rf ~/.vim
+rm -rf ~/.vimd
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+# Install ansible
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y curl git software-properties-common ansible
+
+# pull ansible
+sudo ansible-playbook rebuild.yaml -i 127.0.0.1, --connection=local
+
+sudo apt autoremove -y
+
+
+ln -srf .config/nvim ~/.config/nvim 
